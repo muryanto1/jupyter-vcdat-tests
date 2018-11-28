da48af226221497604e7baa848e16f4ac004c8a3
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-11-28 12:34:46 -0800

    temporarily removed ffmpeg pinning

--- lib/BaseTestCase.py
+++ lib/BaseTestCase.py
@@ -35,7 +35,7 @@ class BaseTestCase(unittest.TestCase):
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument("--disable-popup-blocking")
             chrome_options.add_argument("--start-maximized")
-            chrome_options.add_argument("--headless")
+            #chrome_options.add_argument("--headless")
             preferences = {"download.default_directory": self._download_dir,
                            "directory_upgrade": True,
                            "safebrowsing.enabled": True,
