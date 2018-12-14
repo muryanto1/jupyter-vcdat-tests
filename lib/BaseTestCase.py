a9fa43644b7df941cf1e549afb54c0a9708bca05
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-12-14 13:06:52 -0800

    try RemoteWebDriver

--- lib/BaseTestCase.py
+++ lib/BaseTestCase.py
@@ -51,7 +51,12 @@ class BaseTestCase(unittest.TestCase):
                            "safebrowsing.enabled": True,
                            "prompt_for_download": True}
             chrome_options.add_experimental_option("prefs", preferences)
-            self.driver = webdriver.Chrome(options=chrome_options)
+            #self.driver = webdriver.Chrome(options=chrome_options)
+
+            self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+                                           desired_capabilities=DesiredCapabilities.CHROME,
+                                           options=chrome_options)
+
         elif browser == 'firefox':
             firefox_profile = FirefoxProfile() # profile                                                                            
             firefox_profile.set_preference('extensions.logging.enabled', False)
